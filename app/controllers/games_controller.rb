06a2e98af8798149014054b672b1784fa3d7210e
Thomas Sanders <thomas.a.sanders@gmail.com>
2021-05-17 15:59:05 +0900

    It's detecting if the word is biuldable and valid

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
 
   def new
@@ -5,6 +7,18 @@ class GamesController < ApplicationController
   end
 
   def score
+    @user_word = params[:word].upcase
+    @letters = params[:letters].split(" ")
+    @buildable = @user_word.chars.all? { |letter| @user_word.count(letter) <= @letters.count(letter) }
+    @api_results = URI.open("https://wagon-dictionary.herokuapp.com/#{@user_word}")
+    @json = JSON.parse(@api_results.read)
+    if @buildable == false
+      @response = "Sorry but #{@user_word} can't be built out of #{@letters}."
+    elsif @json["found"] == true
+      @response = "Congratulations! #{@user_word} is a valid English word!"
+    else
+      @response = "Sorry but #{@user_word} does not seem to be a valid English word..."
+    end
   end
 
 end
